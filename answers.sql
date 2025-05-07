d1da88a2db50fcc2073e3c9cdacfb7ab5638b634
jacklinenyaga <jacklinenyaganjoki@gmail.com>
2025-05-07 11:09:54 +0300

    corrected assignment

--- answers.sql
+++ answers.sql
@@ -14,17 +14,19 @@
 
 USE Week7;
 
-SELECT 101 AS OrderID, 'John Doe' AS CustomerName, 'Laptop' AS Product
-UNION ALL
-SELECT 101, 'John Doe', 'Mouse'
-UNION ALL
-SELECT 102, 'Jane Smith', 'Tablet'
-UNION ALL
-SELECT 102, 'Jane Smith', 'Keyboard'
-UNION ALL
-SELECT 102, 'Jane Smith', 'Mouse'
-UNION ALL
-SELECT 103, 'Emily Clark', 'Phone';
+CREATE TABLE ProductDetail (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Products VARCHAR(100)
+);
+INSERT INTO ProductDetail(OrderID, CustomerName, Products)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
 
 -- Question 2 Achieving 2NF (Second Normal Form) 🧩
 
@@ -48,21 +50,22 @@ CREATE TABLE Orders (
     OrderID INT PRIMARY KEY,
     CustomerName VARCHAR(100)
 );
+INSERT INTO Orders (OrderID, CustomerName)
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
 
-CREATE TABLE OrderItems (
+
+CREATE TABLE Product (
     OrderID INT,
     Product VARCHAR(100),
     Quantity INT,
     PRIMARY KEY (OrderID, Product),
     FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
 );
-
-INSERT INTO Orders (OrderID, CustomerName) VALUES
-(101, 'John Doe'),
-(102, 'Jane Smith'),
-(103, 'Emily Clark');
-
-INSERT INTO OrderItems (OrderID, Product, Quantity) VALUES
+INSERT INTO Product (OrderID, Product, Quantity)
+VALUES
 (101, 'Laptop', 2),
 (101, 'Mouse', 1),
 (102, 'Tablet', 3),
